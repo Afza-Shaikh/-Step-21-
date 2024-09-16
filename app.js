bc50b4e7168f06a1e9445e07079532dab32f89f7
afza shaikh <shaikhafza961@gmail.com>
2024-09-16 22:18:27 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+"use strict";
+//two objects
+const vegetables = {
+    name: 'tomato',
+    price: 100
+};
+const fruit = {
+    name: 'mango',
+    price: 200
+};
+console.log(`vegetable name: ${vegetables.name},vegetable price: ${vegetables.price}`);
+console.log(`fruit name: ${fruit.name},fruit price: ${fruit.price}`);
